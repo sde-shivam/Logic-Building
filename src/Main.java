1d932888b72b9d423bba8ce21f6ecbe23b7fd266
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-01-02 08:47:41 +0530

    Floyd's Triangle and 0-1 Triangle Pattern

--- src/Main.java
+++ src/Main.java
@@ -110,9 +110,34 @@ class Main {
              System.out.println();
          }
         }
+        //Floyd's Triangle Pattern--------------------->
+        public static void floydTriangle(int n){
+        int num =1;
+        for(int i=1;i<=n;i++){
+            for(int j =1;j<=i;j++){
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+        }
+
+        //0-1 Triangle ------------------->
+        public static void binTriangle(int n){
+            for(int i=1;i<=n;i++){
+                for(int j =1;j<=i;j++){
+                    if((i+j)%2==0) {
+                        System.out.print("1 ");
+                    }else{
+                        System.out.print("0 ");
+                    }
+                }
+                System.out.println();
+            }
+        }
         public static void main (String args[]){
 
-            invertedHalfPyramid(5);
+            binTriangle(5);
 
         }
     }
