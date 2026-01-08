cba8b2defb90e5fbf15ae0987e464ccc21bc3cdb
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-01-08 08:45:00 +0530

    Binary search

--- src/Main.java
+++ src/Main.java
@@ -308,10 +308,30 @@ class Main {
         }
         return smallest;
     }
+    //Binary Search --------------------->
+    public static int binarySearch(int numbers[],int key){
+        int start =0; int end = numbers.length-1;
+        while(start<=end){
+           int mid = (start+end)/2;
+           if(numbers[mid]==key){
+               return mid;
+           }
+           if(numbers[mid]<key){
+               start = mid+1;
+           }else{
+               end = mid-1;
+           }
+        }
+        return -1;
+    }
+    /* int numbers[]={10,11,12,13,14,15,16,17};
+       int key = 11;
+        int result = binarySearch(numbers,key);
+        System.out.print("Element present at index : "+result); */
+
+
     public static void main (String args[]){
-       int numbers[]={10,93,34,7,23,46,56};
-        int result = getSmallest(numbers);
-        System.out.print("Smallest number is - "+result);
+
     }
 
         }
