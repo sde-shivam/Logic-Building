328f8596b8304566d40779014c900ecb739ea7dd
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-01-09 08:25:14 +0530

    Print the pairs and number of pair in an array

--- src/Main.java
+++ src/Main.java
@@ -348,13 +348,24 @@ class Main {
          for(int i=0;i<numbers.length;i++){
            System.out.print(numbers[i]+" ");
         } */
-    //
-    public static void main (String args[]){
-        int numbers[] = {2,3,4,5,6,7,8};
-        reverseArr(numbers);
+    //print the pairs of a number from array ----------->
+    public static void printPairs(int numbers[]){
+        int tp = 0;
         for(int i=0;i<numbers.length;i++){
-            System.out.print(numbers[i]+" ");
+            int curr =numbers[i];//2,4,6,8,10
+            for(int j=i+1;j<numbers.length;j++){
+                System.out.print("("+curr+","+numbers[j]+")");
+                tp++;
+            }
+            System.out.println();
         }
+        System.out.print("Number of pairs = "+tp);
+    }
+
+    public static void main (String args[]){
+        int numbers[] = {2,4,6,8,10};
+        printPairs(numbers);
+
 
     }
 
