1dc948080e399fb04804ef65766d0ec89f256c62
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-03-27 22:16:04 +0530

    Sort an Array of 0s, 1s and 2s

--- src/Main.java
+++ src/Main.java
@@ -762,9 +762,32 @@ public static void moveZeroes(int[] nums) {
           }
         }
       }
+  // Sort an Array of 0s, 1s and 2s----------------------->
+  public static void sort012(int[] arr) {
+      int low = 0, mid = 0, high = arr.length - 1;
+
+      while (mid <= high) {
+          if (arr[mid] == 0) {
+              int temp = arr[low];
+              arr[low] = arr[mid];
+              arr[mid] = temp;
+              low++;
+              mid++;
+          }
+          else if (arr[mid] == 1) {
+              mid++;
+          }
+          else { // arr[mid] == 2
+              int temp = arr[mid];
+              arr[mid] = arr[high];
+              arr[high] = temp;
+              high--;
+          }
+      }
+  }
 public static void main (String args[]){
      int num[]={1,2,4,5};
-    System.out.print(missingNumber(num));
+    sort012((num));
     }
 
 
