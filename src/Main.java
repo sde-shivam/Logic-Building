cfb4b50d9202524771162f570039ff814d6c462e
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-03-29 19:28:32 +0530

    Best Time to Buy and Sell Stock

--- src/Main.java
+++ src/Main.java
@@ -796,6 +796,23 @@ public static int removeDuplicates(int[] nums) {
     }
     return i + 1;
 }
+// Best Time to Buy and Sell Stock -------------------------------->
+public static int maxProfit(int[] prices) {
+    int minPrice = Integer.MAX_VALUE;
+    int maxProfit = 0;
+
+    for (int price : prices) {
+
+        if (price < minPrice) {
+            minPrice = price; // buy here
+        } else {
+            int profit = price - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+        }
+    }
+    return maxProfit;
+}
+
 public static void main (String args[]){
      int num[]={1,2,4,5};
     sort012((num));
