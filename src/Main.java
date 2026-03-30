791fbf4d46c615ce6213a588a411fe830a7bb0a4
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-03-30 21:07:23 +0530

    Best Time to Buy and Sell Stock 2

--- src/Main.java
+++ src/Main.java
@@ -800,11 +800,9 @@ public static int removeDuplicates(int[] nums) {
 public static int maxProfit(int[] prices) {
     int minPrice = Integer.MAX_VALUE;
     int maxProfit = 0;
-
     for (int price : prices) {
-
         if (price < minPrice) {
-            minPrice = price; // buy here
+            minPrice = price;
         } else {
             int profit = price - minPrice;
             maxProfit = Math.max(maxProfit, profit);
@@ -812,7 +810,23 @@ public static int maxProfit(int[] prices) {
     }
     return maxProfit;
 }
+// Best Time to Buy and Sell Stock 2 --------------------------->
+/*Input: [7,1,5,3,6,4]
+Output: 7
+Explanation:
+Buy at 1 → Sell at 5 = +4
+Buy at 3 → Sell at 6 = +3
+Total = 7*/
+public static int maxProfit2(int[] prices) {
+    int profit = 0;
 
+    for (int i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1];
+        }
+    }
+    return profit;
+}
 public static void main (String args[]){
      int num[]={1,2,4,5};
     sort012((num));
