30bf21ae4512e8cbff1c51616311abcf75066a2d
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-04-30 14:31:19 +0530

    Bubble shorting

--- src/Main.java
+++ src/Main.java
@@ -1422,8 +1422,24 @@ public static void freqOfChar(String s){
 
     }
 }
+//Bubble shorting--------------------->
+public static void bubble(int[]arr){
+    int a = arr.length;
+    for(int i=0;i<a;i++){
+        for(int j=i+1;j<a;j++){
+            if(arr[i]>arr[j]){
+                int temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    for(int k : arr){
+        System.out.print(k+",");
+    }
+}
 public static void main (String args[]){
-    freqOfChar("shivvaram");
+    bubble(new int[]{4, 2, 6, 1, 9,0});
 }
 
 
